32cc5fabac0d6b2a5c09816c59e48d0ffdb7f2a2
tiagoOliveira <89092378+tiago19deoliveira@users.noreply.github.com>
2024-03-06 22:43:29 -0300

    Add files via upload

---
+++ exercício 1 modulo 1/exercício 1 modulo 1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+void QuatroOperacoes()
+{
+    float a = 4f;
+    float b = 7f;
+
+    float adicao = a + b;
+    float subtracao = a - b;
+    float divisao = b / a;
+    float multiplicacao = a * b;
+
+}
+
+QuatroOperacoes();
